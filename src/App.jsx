064f087a7ef12139dabb60a56239d86181f953d9
Viktor Kashtanov <pistoletviktor@gmail.com>
2025-09-19 16:40:13 +0300

    enabling HTTPS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [userState, setUserState] = useState(null);
 
+    const validateSession = async (userId) => {
+        const response = await fetch('https://localhost:8101/api/v1/validator/validate-session', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({userId})
+        });
+
+        return await response.json();
+    };
+
     useEffect(() => {
-  // that global variable is needed here to persist data
+        // that global variable is needed here to persist data
         const userData = window.TEST_USER_DATA;
 
         if (userData && userData.userId !== "20130") { // is the default Liferay GUEST USER ID = 20130, to distinguish
@@ -17,9 +30,15 @@ function App() {
 
     if (!userState) return <div>User is not authorized or there is a problem with data retrieving.</div>;
 
+
+    validateSession(userState.userId).then((user) => {
+        console.log("Was send to SPRING" + user)
+    })
+
+
     return (
 
-        <div style={{ padding: '20px' }}>
+        <div style={{padding: '20px'}}>
             <h1>!_REACT_HEADER_!</h1>
             <h8>AppJSX Loaded here</h8>
 
